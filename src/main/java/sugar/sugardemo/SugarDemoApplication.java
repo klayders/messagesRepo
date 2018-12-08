041d61323bcad9b002efa7ccf913fe8e07a9e080
Jura Kuznetsov <matrocob@gmail.com>
2018-12-08 17:55:25 +0300

    'Starting'

---
+++ src/main/java/sugar/sugardemo/SugarDemoApplication.java
@@ -0,0 +1,12 @@
+package sugar.sugardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugarDemoApplication.class, args);
+	}
+}
