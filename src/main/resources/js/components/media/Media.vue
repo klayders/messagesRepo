c91b42fda74b6d16c733a99adca0d3b2005f3f2f
jura <yk@finch-melrose.com>
2019-10-20 20:45:24 +0300

    refactoring

--- src/main/resources/js/components/media/Media.vue
+++ src/main/resources/js/components/media/Media.vue
@@ -1,26 +1,26 @@
 <template>
-    <v-card>
-        <v-flex v-if="type === 'href'" xs12 sm6 offset-sm3>
-            <v-img v-if="message.linkCover" :src="message.linkCover" aspect-ratio="2.75"></v-img>
-            <v-card-title>
-                <div>
-                    <h3>
-                        <a :href="message.link">{{message.linkTitle || message.link}}</a>
-                    </h3>
-                    <div v-if="message.linkDescription">{{message.linkDescription}}</div>
-                </div>
-            </v-card-title>
-        </v-flex>
-        <v-flex v-if="type === 'image'" xs12 sm6 offset-sm3>
-            <a :href="message.link">
-                <v-img v-if="message.linkCover" :src="message.linkCover" aspect-ratio="2.75"></v-img>
-                {{message.link}}
-            </a>
-        </v-flex>
-        <v-flex v-if="type === 'youtube'" xs12 sm6 offset-sm3>
-            <you-tube :src="message.link"></you-tube>
-        </v-flex>
-    </v-card>
+  <v-card>
+    <v-flex v-if="type === 'href'" xs12 sm6 offset-sm3>
+      <v-img v-if="message.linkCover" :src="message.linkCover" aspect-ratio="2.75"></v-img>
+      <v-card-title>
+        <div>
+          <h3>
+            <a :href="message.link">{{message.linkTitle || message.link}}</a>
+          </h3>
+          <div v-if="message.linkDescription">{{message.linkDescription}}</div>
+        </div>
+      </v-card-title>
+    </v-flex>
+    <v-flex v-if="type === 'image'" xs12 sm6 offset-sm3>
+      <a :href="message.link">
+        <v-img v-if="message.linkCover" :src="message.linkCover" aspect-ratio="2.75"></v-img>
+        {{message.link}}
+      </a>
+    </v-flex>
+    <v-flex v-if="type === 'youtube'" xs12 sm6 offset-sm3>
+      <you-tube :src="message.link"></you-tube>
+    </v-flex>
+  </v-card>
 </template>
 
 <script>
--- src/main/resources/js/components/media/Youtube.vue
+++ src/main/resources/js/components/media/Youtube.vue
@@ -1,12 +1,12 @@
 <template>
-    <iframe
-            width="560"
-            height="315"
-            :src="targetSrc"
-            frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-    ></iframe>
+  <iframe
+    width="560"
+    height="315"
+    :src="targetSrc"
+    frameborder="0"
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
 </template>
 
 <script>
